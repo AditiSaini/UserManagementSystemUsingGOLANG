7d5d86b045f3fceeb572e14419dbb02fd946befe
Aditi Saini <aditi.saini@shopee.com>
2021-01-12 17:22:24 +0800

    Updated handler functions

--- Servers/HTTPServer/httpS.go
+++ Servers/HTTPServer/httpS.go
@@ -5,55 +5,6 @@ import (
 	"net/http"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
-	}
-	w.Write([]byte("Hello from Home page"))
-}
-
-//Login handler function
-func loginUser(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Logging in user..."))
-}
-
-//Logout handler function
-func logoutUser(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Logging out user..."))
-}
-
-//Show profile handler function
-func showProfile(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Displaying profile..."))
-}
-
-//Modify profile handler function
-func updateProfile(w http.ResponseWriter, r *http.Request) {
-	// Use r.Method to check whether the request is using POST or not. Note that
-	// http.MethodPost is a constant equal to the string "POST".
-	if r.Method != http.MethodPost {
-		// If it's not, use the w.WriteHeader() method to send a 405 status
-		// code and the w.Write() method to write a "Method Not Allowed"
-		// response body. We then return from the function so that the
-		// subsequent code is not executed.
-		w.WriteHeader(405)
-		w.Write([]byte("Method Not Allowed"))
-		return
-	}
-	w.Write([]byte("Modify user profile..."))
-}
-
-//Upload profile picture
-func uploadPicture(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		w.WriteHeader(405)
-		w.Write([]byte("Method Not Allowed"))
-		return
-	}
-	w.Write([]byte("Uploading profile picture..."))
-}
-
 func main() {
 	//Registering the 2 new handler functions and corresponding URL
 	//patterns with the servemux
